498f931c2a1edc86889819e78a59aa441ce6a319
Aminah Burch <hello@aminah.io>
2021-08-01 01:20:19 +0300

    Added the functions to index.js; added some tailwind styling; added an env v; updated njs config file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,42 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from '../src/artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = process.env.CONTRACT_ADDRESS;
 
 export default function Home() {
+  const [greeting, setGreetingValue] = useState('');
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+      try {
+        const data = await contract.greet();
+        console.log('data: ', data);
+      } catch (err) {
+        console.log('Error: ', err)
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
   return (
     <div className='container w-96 mx-auto'>
       <Head>
@@ -8,15 +44,14 @@ export default function Home() {
         <meta name="description" content="Ethereum fun" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-        <div className="sm:text-center lg:text-left">
-          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">Ethereum</span>
-            <span className="block text-indigo-600 xl:inline">Smorgaasborg</span>
-          </h1>
-          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-          </p>
+      <main className="mt-10 mx-auto text-center max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <h1 className='text-8xl md:text-5xl sm:text-4xl xs:text-3xl'>The <span className='font-bold text-indigo-500'>AIO</span> Token</h1>
+        <div className='container mx-auto box-content h-40'>
+          <div className='flex-col'>
+            <button className='bg-indigo-800 w-48 h-10 text-white rounded-sm' onClick={fetchGreeting}>Fetch Greeting</button>
+            <button onClick={setGreeting}>Set Greeting</button>
+            <input onChange={e => setGreetingValue(e.target.value)} placeholder='Set greeting' />
+          </div>
         </div>
       </main>
     </div>
